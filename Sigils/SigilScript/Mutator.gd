1342cac183c6cd7940565cffbf7e9d7804fbd48f
0ne5quared <sanchit.sharma@education.nsw.gov.au>
2026-07-14 22:40:02 +1000

    Update Mutator.gd

--- Sigils/SigilScript/Mutator.gd
+++ Sigils/SigilScript/Mutator.gd
@@ -13,26 +13,3 @@ func handle_event(event: String, params: Array):
     #		card.calculate_buffs()
     #	for eCard in slotManager.all_enemy_cards():
     #		eCard.calculate_buffs()
-				
-
-"""
-extends SigilEffect
-
-#Used for sigils that do something at the start of the turn
-func start_of_turn(card_anim):
-	card_anim.play("Evolve")
-	yield (card_anim, "animation_finished")
-	var dmgTaken = card.card_data["health"] - card.health
-	card.from_data(CardInfo.from_name(card.card_data["evolution"]))
-	card.health = card.card_data["health"] - dmgTaken
-	# Calculate buffs
-	slotManager.recalculate_buffs_and_such()
-#	for card in slotManager.all_friendly_cards():
-#		card.calculate_buffs()
-#	for eCard in slotManager.all_enemy_cards():
-#		eCard.calculate_buffs()
-
-
-func priority():
-	return 1
-"""
