207b5eb9b831530102209b6bfa7f34acb1944227
0ne5quared <sanchit.sharma@education.nsw.gov.au>
2026-07-15 02:14:02 +1000

    Update Mutator.gd

--- Sigils/SigilScript/Mutator.gd
+++ Sigils/SigilScript/Mutator.gd
@@ -7,9 +7,5 @@ func handle_event(event: String, params: Array):
 		print("HIIHIIHIHI")
 		var dmgTaken = card.card_data["health"] - card.health
 		card.from_data(CardInfo.from_name(card.card_data["evolution"]))
-    	card.health = card.card_data["health"] - dmgTaken
-    	slotManager.recalculate_buffs_and_such()
-    #	for card in slotManager.all_friendly_cards():
-    #		card.calculate_buffs()
-    #	for eCard in slotManager.all_enemy_cards():
-    #		eCard.calculate_buffs()
+		card.health = card.card_data["health"] - dmgTaken
+		slotManager.recalculate_buffs_and_such()
