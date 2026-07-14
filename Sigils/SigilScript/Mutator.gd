662cf88f111ab440ab3fa4d652ca025fc89d9dd3
0ne5quared <sanchit.sharma@education.nsw.gov.au>
2026-07-15 01:30:08 +1000

    Update Mutator.gd

--- Sigils/SigilScript/Mutator.gd
+++ Sigils/SigilScript/Mutator.gd
@@ -3,7 +3,7 @@ extends SigilEffect
 # This is called whenever something happens that might trigger a sigil, with 'event' representing what happened
 func handle_event(event: String, params: Array):
 
-	if event == "card_summoned" and not card.in_hand and params[0] == is_friendly:
+	if event == "card_summoned" and not card.in_hand and (params[0].get_parent().get_parent().name == "PlayerSlots") == isFriendly:
     	var dmgTaken = card.card_data["health"] - card.health
     	card.from_data(CardInfo.from_name(card.card_data["evolution"]))
     	card.health = card.card_data["health"] - dmgTaken
