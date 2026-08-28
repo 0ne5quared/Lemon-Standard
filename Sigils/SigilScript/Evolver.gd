7f059fbe6b300b65367ab931e6f353e9bbc4b520
0ne5quared <sanchit.sharma@education.nsw.gov.au>
2026-08-29 01:46:44 +1000

    Update Evolver.gd

--- Sigils/SigilScript/Evolver.gd
+++ Sigils/SigilScript/Evolver.gd
@@ -5,7 +5,8 @@ func handle_event(event: String, params: Array):
 	# attached_card_summoned represents the card bearing the sigil being summoned
 	if event == "card_perished" and params[0] == card and isFriendly:
 		print("EVOLVERS")
-		print("Health = "+ card.health)
+		print("Health")
+		print(str(card.health))
 		if card.health != 0:
 			# Draw the modified card copy
 			fightManager.draw_card(CardInfo.from_name(card.card_data["evolution"]))
