286ec9d41a841b7e84e9e9f3c98a331484efc528
0ne5quared <sanchit.sharma@education.nsw.gov.au>
2026-08-29 01:34:42 +1000

    Update Evolver.gd

--- Sigils/SigilScript/Evolver.gd
+++ Sigils/SigilScript/Evolver.gd
@@ -1,9 +1,7 @@
 extends SigilEffect
 
 # This is called whenever something happens that might trigger a sigil, with 'event' representing what happened
-
 func handle_event(event: String, params: Array):
-
 	# attached_card_summoned represents the card bearing the sigil being summoned
 	if event == "card_perished" and params[0] == card and isFriendly:
 		print("Health = "+ card.health)
