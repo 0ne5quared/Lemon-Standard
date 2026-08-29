18bcfc5c1e66d1975e3290eda42ff35a815c75b5
0ne5quared <sanchit.sharma@education.nsw.gov.au>
2026-08-30 03:14:05 +1000

    Update Evolver.gd

--- Sigils/SigilScript/Evolver.gd
+++ Sigils/SigilScript/Evolver.gd
@@ -3,9 +3,13 @@ extends SigilEffect
 # This is called whenever something happens that might trigger a sigil, with 'event' representing what happened
 func handle_event(event: String, params: Array):
 	# attached_card_summoned represents the card bearing the sigil being summoned
-	if event == "card_perished" and params[0] == card and isFriendly:
+	if event == "card_perished" and params[0] == card:
 		print("Health")
 		print(str(card.health))
 		if card.get_node("CardBody/SacOlay").visible:
 			# Draw the modified card copy
-			fightManager.draw_card(CardInfo.from_name(card.card_data["evolution"]))
+			if isFriendly:
+				fightManager.draw_card(CardInfo.from_name(card.card_data["evolution"]))
+				fightManager.add_bones(-1)
+			else:
+				fightManager.add_opponent_bones(-1)
